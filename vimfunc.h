3296cc91efd4ea50aae84741e9a13da60a6deda9
Winly <lianghao_2009@126.com>
2020-06-25 20:37:56 +0800

    fix some insert_mode bug(without backspace)

--- vimfunc.h
+++
@@ -1,6 +0,0 @@
-#include "types.h"
-word* addnode(struct word *,int, int,uchar);
-word* create_text(int, uchar *);
-int readfile(char *, struct text *);
-int writefile(char *, struct word *);
-int printwords(struct word *, int);
